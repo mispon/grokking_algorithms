6c50a994d6c2c941ee39a98e11f2e25404ae5635
bartem <bartem@ozon.ru>
2022-06-15 21:31:51 +0300

    added binary search impl

---
+++ binary_search/search.go
@@ -0,0 +1,25 @@
+package binary_search
+
+func search(values []int, v int) (int, bool) {
+	var (
+		low  = 0
+		high = len(values) - 1
+	)
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := values[mid]
+
+		if guess == v {
+			return mid, true
+		}
+
+		if guess < v {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1, false
+}
