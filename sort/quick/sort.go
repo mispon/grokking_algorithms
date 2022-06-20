c5a80306cf020296f6ccc6e2a33342816f9ea730
bartem <bartem@ozon.ru>
2022-06-20 23:04:06 +0300

    added quick sort and tests

--- sort/quick/sort.go
+++ sort/quick/sort.go
@@ -1,5 +1,38 @@
 package quick
 
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func Sort(values []int) []int {
+	if len(values) < 2 {
+		return values
+	}
+
+	var (
+		left  = 0
+		right = len(values) - 1
+		pivot = rand.Int() % len(values)
+	)
+	
+	values[pivot], values[right] = values[right], values[pivot]
+
+	for i := range values {
+		if values[i] < values[right] {
+			values[left], values[i] = values[i], values[left]
+			left++
+		}
+	}
+
+	values[left], values[right] = values[right], values[left]
+
+	Sort(values[:left])
+	Sort(values[left+1:])
+
 	return values
 }
