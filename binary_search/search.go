aefb686a3f783de37ea8ce03a7829c1b48de8fc8
bartem <bartem@ozon.ru>
2022-06-20 21:41:30 +0300

    added recursive slice funcs and binary rsearch

--- binary_search/search.go
+++ binary_search/search.go
@@ -23,3 +23,24 @@ func search(values []int, v int) (int, bool) {
 
 	return 0, false
 }
+
+func rsearch(values []int, v int, low, high int) (int, bool) {
+	if low > high {
+		return 0, false
+	}
+
+	mid := (low + high) / 2
+	guess := values[mid]
+
+	if guess == v {
+		return mid, true
+	}
+
+	if guess < v {
+		low = mid + 1
+	} else {
+		high = mid - 1
+	}
+
+	return rsearch(values, v, low, high)
+}
--- binary_search/search_test.go
+++ binary_search/search_test.go
@@ -54,13 +54,67 @@ func Test_search(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run("", func(t *testing.T) {
-			// arrange
 			values := selection.Sort(tc.values)
 
-			// act
 			idx, ok := search(values, tc.item)
 
-			// assert
+			require.Equal(t, tc.expIndex, idx)
+			require.Equal(t, tc.expBool, ok)
+		})
+	}
+}
+
+func Test_rsearch(t *testing.T) {
+	testCases := []struct {
+		values   []int
+		item     int
+		expIndex int
+		expBool  bool
+	}{
+		{
+			values:   []int{3, 5, 1, 9, 7},
+			item:     3,
+			expIndex: 1,
+			expBool:  true,
+		},
+		{
+			values:   []int{3, 5, 1, 9, 7},
+			item:     6,
+			expIndex: 0,
+			expBool:  false,
+		},
+		{
+			values:   []int{1, 2, 3, 4, 5},
+			item:     3,
+			expIndex: 2,
+			expBool:  true,
+		},
+		{
+			values:   []int{1, 1, 3, 4, 5},
+			item:     1,
+			expIndex: 0,
+			expBool:  true,
+		},
+		{
+			values:   []int{1, 1, 1, 4, 5},
+			item:     1,
+			expIndex: 2,
+			expBool:  true,
+		},
+		{
+			values:   []int{4, 1, 5, 1, 1, 8},
+			item:     1,
+			expIndex: 1,
+			expBool:  true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			values := selection.Sort(tc.values)
+
+			idx, ok := rsearch(values, tc.item, 0, len(values))
+
 			require.Equal(t, tc.expIndex, idx)
 			require.Equal(t, tc.expBool, ok)
 		})
